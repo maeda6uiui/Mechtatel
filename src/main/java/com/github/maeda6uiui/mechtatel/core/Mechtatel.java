57f0ebcca6d946b7a7a81af53b45f1a0e015792f
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-07-22 21:07:51 +0900

    Add Mechtatel.java

---
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/Mechtatel.java
@@ -0,0 +1,28 @@
+package com.github.maeda6uiui.mechtatel.core;
+
+/**
+ * Base class of the Mechtatel engine
+ *
+ * @author maeda
+ */
+public class Mechtatel {
+    public void init() {
+
+    }
+
+    public void dispose() {
+
+    }
+
+    public void reshape(int width, int height) {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+        
+    }
+}

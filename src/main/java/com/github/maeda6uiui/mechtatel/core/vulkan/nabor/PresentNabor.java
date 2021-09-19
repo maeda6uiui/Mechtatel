58a0a500046c351d17ef96c8cefe9a0f7e091ed0
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-09-19 17:19:28 +0900

    Remove textureDstBinding

--- src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PresentNabor.java
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PresentNabor.java
@@ -19,22 +19,12 @@ import static org.lwjgl.vulkan.VK10.*;
  * @author maeda
  */
 public class PresentNabor extends Nabor {
-    private int textureDstBinding;
     private long textureSampler;
 
     public PresentNabor(VkDevice device) {
         super(device);
 
-        textureDstBinding = 0;
         textureSampler = TextureSamplerCreator.createTextureSampler(device);
-    }
-
-    public int getTextureDstBinding() {
-        return textureDstBinding;
-    }
-
-    protected void setTextureDstBinding(int textureDstBinding) {
-        this.textureDstBinding = textureDstBinding;
     }
 
     public long getTextureSampler() {
@@ -347,7 +337,7 @@ public class PresentNabor extends Nabor {
 
             VkWriteDescriptorSet.Buffer samplerDescriptorWrite = VkWriteDescriptorSet.callocStack(1, stack);
             samplerDescriptorWrite.sType(VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
-            samplerDescriptorWrite.dstBinding(textureDstBinding);
+            samplerDescriptorWrite.dstBinding(0);
             samplerDescriptorWrite.dstArrayElement(0);
             samplerDescriptorWrite.descriptorType(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
             samplerDescriptorWrite.descriptorCount(1);

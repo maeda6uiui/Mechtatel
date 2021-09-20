9ec33c0d916bce9132ca759b259e0a0896b2d7d6
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-09-20 09:29:59 +0900

    Rename arguments

--- src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/util/ImageUtils.java
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/util/ImageUtils.java
@@ -23,8 +23,8 @@ public class ImageUtils {
             int tiling,
             int usage,
             int memProperties,
-            LongBuffer pTextureImage,
-            LongBuffer pTextureImageMemory) {
+            LongBuffer pImage,
+            LongBuffer pImageMemory) {
         try (MemoryStack stack = MemoryStack.stackPush()) {
             VkImageCreateInfo imageInfo = VkImageCreateInfo.callocStack(stack);
             imageInfo.sType(VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO);
@@ -41,23 +41,23 @@ public class ImageUtils {
             imageInfo.samples(numSamples);
             imageInfo.sharingMode(VK_SHARING_MODE_EXCLUSIVE);
 
-            if (vkCreateImage(device, imageInfo, null, pTextureImage) != VK_SUCCESS) {
+            if (vkCreateImage(device, imageInfo, null, pImage) != VK_SUCCESS) {
                 throw new RuntimeException("Failed to create an image");
             }
 
             VkMemoryRequirements memRequirements = VkMemoryRequirements.mallocStack(stack);
-            vkGetImageMemoryRequirements(device, pTextureImage.get(0), memRequirements);
+            vkGetImageMemoryRequirements(device, pImage.get(0), memRequirements);
 
             VkMemoryAllocateInfo allocInfo = VkMemoryAllocateInfo.callocStack(stack);
             allocInfo.sType(VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO);
             allocInfo.allocationSize(memRequirements.size());
             allocInfo.memoryTypeIndex(MemoryUtils.findMemoryType(device, memRequirements.memoryTypeBits(), memProperties));
 
-            if (vkAllocateMemory(device, allocInfo, null, pTextureImageMemory) != VK_SUCCESS) {
+            if (vkAllocateMemory(device, allocInfo, null, pImageMemory) != VK_SUCCESS) {
                 throw new RuntimeException("Failed to allocate an image memory");
             }
 
-            vkBindImageMemory(device, pTextureImage.get(0), pTextureImageMemory.get(0), 0);
+            vkBindImageMemory(device, pImage.get(0), pImageMemory.get(0), 0);
         }
     }
 

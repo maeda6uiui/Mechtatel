2bae171ed79dfdc868d5e7f0f6ace12719d4da2b
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-09-22 16:59:24 +0900

    Update CameraUBO

--- src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/ubo/CameraUBO.java
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/ubo/CameraUBO.java
@@ -3,6 +3,7 @@ package com.github.maeda6uiui.mechtatel.core.vulkan.ubo;
 import com.github.maeda6uiui.mechtatel.core.camera.Camera;
 import com.github.maeda6uiui.mechtatel.core.vulkan.util.AlignmentUtils;
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.joml.Vector3fc;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
@@ -20,14 +21,20 @@ import static org.lwjgl.vulkan.VK10.vkUnmapMemory;
  * @author maeda
  */
 public class CameraUBO {
-    public static final int SIZEOF = 2 * 16 * Float.BYTES;
+    public static final int SIZEOF = (2 * 16 + 2 * 3) * Float.BYTES;
 
     public Matrix4f view;
     public Matrix4f proj;
 
+    public Vector3f eye;
+    public Vector3f center;
+
     public CameraUBO() {
         view = new Matrix4f();
         proj = new Matrix4f();
+        
+        eye = new Vector3f();
+        center = new Vector3f();
     }
 
     public CameraUBO(Camera camera) {
@@ -45,13 +52,19 @@ public class CameraUBO {
                 up.x(), up.y(), up.z());
         proj = new Matrix4f().perspective(fovY, aspect, zNear, zFar);
         proj.m11(proj.m11() * (-1.0f));
+
+        this.eye = new Vector3f(eye);
+        this.center = new Vector3f(center);
     }
 
     private void memcpy(ByteBuffer buffer) {
-        final int mat4size = 16 * Float.BYTES;
+        final int mat4Size = 16 * Float.BYTES;
+        final int vec3Size = 3 * Float.BYTES;
 
         view.get(0, buffer);
-        proj.get(AlignmentUtils.alignas(mat4size, AlignmentUtils.alignof(proj)), buffer);
+        proj.get(AlignmentUtils.alignas(mat4Size, AlignmentUtils.alignof(proj)), buffer);
+        eye.get(AlignmentUtils.alignas(mat4Size * 2, AlignmentUtils.alignof(eye)), buffer);
+        center.get(AlignmentUtils.alignas(mat4Size * 2 + vec3Size, AlignmentUtils.alignof(center)), buffer);
 
         buffer.rewind();
     }

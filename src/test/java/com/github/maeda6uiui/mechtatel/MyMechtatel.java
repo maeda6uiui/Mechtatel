001ffb0ef9fbba1b1f9f46a2689c20d5e35d1cc8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2022-09-11 18:29:40 +0900

    Update

--- src/test/java/com/github/maeda6uiui/mechtatel/MyMechtatel.java
+++ src/test/java/com/github/maeda6uiui/mechtatel/MyMechtatel.java
@@ -5,7 +5,6 @@ import com.github.maeda6uiui.mechtatel.core.MttSettings;
 import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
 import com.github.maeda6uiui.mechtatel.core.component.Vertex2D;
 import org.joml.Vector2f;
-import org.joml.Vector3f;
 import org.joml.Vector4f;
 
 import java.io.IOException;
@@ -33,18 +32,9 @@ public class MyMechtatel extends Mechtatel {
 
     @Override
     public void init() {
-        var capsule = this.createCapsule3D(
-                new Vector3f(0.0f, 0.0f, 0.0f),
-                2.0f,
-                2.0f,
-                32,
-                32,
-                new Vector4f(1.0f, 1.0f, 1.0f, 1.0f)
-        );
-        var axes = this.createPositiveAxesLine3DSet(10.0f);
         var line = this.createLine2D(
-                new Vertex2D(new Vector2f(-1.0f, -1.0f), new Vector4f(1.0f, 0.0f, 0.0f, 1.0f)),
-                new Vertex2D(new Vector2f(1.0f, 1.0f), new Vector4f(1.0f, 0.0f, 0.0f, 1.0f)),
+                new Vertex2D(new Vector2f(-0.5f, 0.0f), new Vector4f(1.0f, 0.0f, 0.0f, 1.0f)),
+                new Vertex2D(new Vector2f(0.5f, 0.0f), new Vector4f(0.0f, 0.0f, 1.0f, 1.0f)),
                 0.0f
         );
 

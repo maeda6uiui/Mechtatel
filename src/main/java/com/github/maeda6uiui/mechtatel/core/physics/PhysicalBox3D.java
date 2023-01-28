c8413fbcefe060a789358613b11f69acfd7ef840
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-01-28 15:44:17 +0900

    Add code

---
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalBox3D.java
@@ -0,0 +1,22 @@
+package com.github.maeda6uiui.mechtatel.core.physics;
+
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.math.Vector3f;
+
+/**
+ * Box 3D
+ *
+ * @author maeda
+ */
+public class PhysicalBox3D extends PhysicalObject3D {
+    public PhysicalBox3D(float halfExtent, float mass) {
+        var shape = new BoxCollisionShape(halfExtent);
+        var body = new PhysicsRigidBody(shape, mass);
+        PhysicalBox3D.getPhysicsSpace().addCollisionObject(body);
+        body.setPhysicsLocation(new Vector3f(0.0f, 0.0f, 0.0f));
+
+        this.setShape(shape);
+        this.setBody(body);
+    }
+}
---
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalCapsule3D.java
@@ -0,0 +1,22 @@
+package com.github.maeda6uiui.mechtatel.core.physics;
+
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.math.Vector3f;
+
+/**
+ * 3D capsule
+ *
+ * @author maeda
+ */
+public class PhysicalCapsule3D extends PhysicalObject3D {
+    public PhysicalCapsule3D(float radius, float height, float mass) {
+        var shape = new CapsuleCollisionShape(radius, height);
+        var body = new PhysicsRigidBody(shape, mass);
+        PhysicalCapsule3D.getPhysicsSpace().addCollisionObject(body);
+        body.setPhysicsLocation(new Vector3f(0.0f, 0.0f, 0.0f));
+
+        this.setShape(shape);
+        this.setBody(body);
+    }
+}

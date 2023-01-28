031541d45c93cdbd73e0cdc2e9f5c9398555c4cd
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-01-28 15:55:04 +0900

    Update

--- src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalBox3D.java
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalBox3D.java
@@ -10,27 +10,17 @@ import com.jme3.math.Vector3f;
  * @author maeda
  */
 public class PhysicalBox3D extends PhysicalObject3D {
-    private BoxCollisionShape createShape(float xHalfExtent, float yHalfExtent, float zHalfExtent) {
-        return new BoxCollisionShape(xHalfExtent, yHalfExtent, zHalfExtent);
+    public PhysicalBox3D(float xHalfExtent, float yHalfExtent, float zHalfExtent, float mass) {
+        var shape = new BoxCollisionShape(xHalfExtent, yHalfExtent, zHalfExtent);
+        var body = new PhysicsRigidBody(shape, mass);
+        PhysicalBox3D.getPhysicsSpace().addCollisionObject(body);
+        body.setPhysicsLocation(new Vector3f(0.0f, 0.0f, 0.0f));
+
+        this.setShape(shape);
+        this.setBody(body);
     }
 
     public PhysicalBox3D(float halfExtent, float mass) {
-        var shape = this.createShape(halfExtent, halfExtent, halfExtent);
-        var body = new PhysicsRigidBody(shape, mass);
-        PhysicalBox3D.getPhysicsSpace().addCollisionObject(body);
-        body.setPhysicsLocation(new Vector3f(0.0f, 0.0f, 0.0f));
-
-        this.setShape(shape);
-        this.setBody(body);
-    }
-
-    public PhysicalBox3D(float xHalfExtent, float yHalfExtent, float zHalfExtent, float mass) {
-        var shape = this.createShape(xHalfExtent, yHalfExtent, zHalfExtent);
-        var body = new PhysicsRigidBody(shape, mass);
-        PhysicalBox3D.getPhysicsSpace().addCollisionObject(body);
-        body.setPhysicsLocation(new Vector3f(0.0f, 0.0f, 0.0f));
-
-        this.setShape(shape);
-        this.setBody(body);
+        this(halfExtent, halfExtent, halfExtent, mass);
     }
 }

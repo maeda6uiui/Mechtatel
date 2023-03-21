1f82ed41368fa375d083d35ed2251a9d7b7b7606
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-03-21 12:52:36 +0900

    Enable blending in PrimitiveNabor

--- src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PrimitiveNabor.java
+++ src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PrimitiveNabor.java
@@ -513,7 +513,13 @@ public class PrimitiveNabor extends Nabor {
                                 VK_COLOR_COMPONENT_G_BIT |
                                 VK_COLOR_COMPONENT_B_BIT |
                                 VK_COLOR_COMPONENT_A_BIT);
-                colorBlendAttachment.blendEnable(false);
+                colorBlendAttachment.blendEnable(true);
+                colorBlendAttachment.srcColorBlendFactor(VK_BLEND_FACTOR_SRC_ALPHA);
+                colorBlendAttachment.dstColorBlendFactor(VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
+                colorBlendAttachment.colorBlendOp(VK_BLEND_OP_ADD);
+                colorBlendAttachment.srcAlphaBlendFactor(VK_BLEND_FACTOR_SRC_ALPHA);
+                colorBlendAttachment.dstAlphaBlendFactor(VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
+                colorBlendAttachment.alphaBlendOp(VK_BLEND_OP_SUBTRACT);
             }
 
             VkPipelineColorBlendStateCreateInfo colorBlending = VkPipelineColorBlendStateCreateInfo.calloc(stack);

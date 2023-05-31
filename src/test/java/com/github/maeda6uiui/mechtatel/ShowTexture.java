3f472da393a36ac5a34671b659f9747505a917e9
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-05-31 09:41:14 +0900

    Add test code

---
+++ src/test/java/com/github/maeda6uiui/mechtatel/ShowTexture.java
@@ -0,0 +1,50 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.DrawPath;
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.ScreenCreator;
+import com.github.maeda6uiui.mechtatel.core.component.MttTexturedQuad2D;
+import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import org.joml.Vector2f;
+
+import java.io.IOException;
+
+public class ShowTexture extends Mechtatel {
+    public ShowTexture(MttSettings settings) {
+        super(settings);
+    }
+
+    public static void main(String[] args) {
+        MttSettings settings;
+        try {
+            settings = new MttSettings("./Mechtatel/Setting/settings.json");
+        } catch (IOException e) {
+            System.out.println("Failed to load setting file. Use default settings");
+            settings = new MttSettings();
+        }
+
+        new ShowTexture(settings);
+    }
+
+    private MttScreen mainScreen;
+    private MttTexturedQuad2D texturedQuad;
+
+    @Override
+    public void init() {
+        var mainScreenCreator = new ScreenCreator(this, "main");
+        mainScreen = mainScreenCreator.create();
+
+        var drawPath = new DrawPath(this);
+        drawPath.addToScreenDrawOrder("main");
+        drawPath.setPresentScreenName("main");
+        drawPath.apply();
+
+        texturedQuad = this.createTexturedQuad2D(
+                "main",
+                "./Mechtatel/Model/Skybox/cubemap.png",
+                new Vector2f(-1.0f, -1.0f),
+                new Vector2f(1.0f, 1.0f),
+                0.0f);
+    }
+}

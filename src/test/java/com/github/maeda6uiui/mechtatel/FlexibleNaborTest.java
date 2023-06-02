fa4ee4b74e770acf7c372b512a5e6e2c6f1fee7e
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-02 11:34:43 +0900

    Update test code

--- src/test/java/com/github/maeda6uiui/mechtatel/FlexibleNaborTest.java
+++ src/test/java/com/github/maeda6uiui/mechtatel/FlexibleNaborTest.java
@@ -1,5 +1,6 @@
 package com.github.maeda6uiui.mechtatel;
 
+import com.github.maeda6uiui.mechtatel.core.DrawPath;
 import com.github.maeda6uiui.mechtatel.core.Mechtatel;
 import com.github.maeda6uiui.mechtatel.core.MttSettings;
 import com.github.maeda6uiui.mechtatel.core.ScreenCreator;
@@ -7,6 +8,7 @@ import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
 import com.github.maeda6uiui.mechtatel.core.component.MttModel3D;
 import com.github.maeda6uiui.mechtatel.core.nabor.FlexibleNaborInfo;
 import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import org.joml.Vector3f;
 
 import java.io.IOException;
 
@@ -30,6 +32,7 @@ public class FlexibleNaborTest extends Mechtatel {
     private MttScreen mainScreen;
     private MttModel3D plane;
     private MttModel3D teapot;
+    private MttModel3D cube;
     private FreeCamera camera;
 
     @Override
@@ -46,12 +49,24 @@ public class FlexibleNaborTest extends Mechtatel {
 
         mainScreen = screenCreator.create();
 
+        mainScreen.createParallelLight();
+        mainScreen.getFog().setStart(10.0f);
+        mainScreen.getFog().setEnd(20.0f);
+
+        var drawPath = new DrawPath(this);
+        drawPath.addToScreenDrawOrder("main");
+        drawPath.setPresentScreenName("main");
+        drawPath.apply();
+
         try {
             plane = this.createModel3D("main", "./Mechtatel/Standard/Model/Plane/plane.obj");
             teapot = this.createModel3D("main", "./Mechtatel/Standard/Model/Teapot/teapot.obj");
+            cube = this.createModel3D("main", "./Mechtatel/Standard/Model/Cube/cube.obj");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        cube.translate(new Vector3f(0.0f, 3.0f, 0.0f));
 
         camera = new FreeCamera(mainScreen.getCamera());
     }
@@ -70,5 +85,16 @@ public class FlexibleNaborTest extends Mechtatel {
                 this.getKeyboardPressingCount("LEFT"),
                 this.getKeyboardPressingCount("RIGHT")
         );
+    }
+
+    @Override
+    public void postPresent() {
+        if (this.getKeyboardPressingCount("ENTER") == 1) {
+            try {
+                this.saveScreenshot("main", "bgra", "screenshot.png");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

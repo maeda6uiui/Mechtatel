ed6477670a7b76eb9e843447141a87bba5ad2550
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-02 15:48:44 +0900

    Add test code

---
+++ src/test/java/com/github/maeda6uiui/mechtatel/CheckboxTest.java
@@ -0,0 +1,41 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class CheckboxTest extends Mechtatel {
+    public CheckboxTest(MttSettings settings) {
+        super(settings);
+    }
+
+    public static void main(String[] args) {
+        MttSettings settings;
+        try {
+            settings = new MttSettings("./Mechtatel/Setting/settings.json");
+        } catch (IOException e) {
+            System.out.println("Failed to load setting file. Use default settings");
+            settings = new MttSettings();
+        }
+
+        new CheckboxTest(settings);
+    }
+
+    @Override
+    public void init() {
+        this.createCheckbox(
+                -0.9f, -0.9f, 0.9f, 0.1f,
+                -0.9f, -0.9f, 0.05f, 0.1f, -0.8f, -0.93f,
+                "This is a sample checkbox", Font.SANS_SERIF, Font.PLAIN, 64,
+                Color.GREEN, Color.WHITE
+        );
+        this.createCheckbox(
+                -0.9f, -0.75f, 0.9f, 0.1f,
+                -0.9f, -0.75f, 0.05f, 0.1f, -0.8f, -0.78f,
+                "これはチェックボックスのサンプルです", Font.SANS_SERIF, Font.PLAIN, 64,
+                Color.ORANGE, Color.GREEN
+        );
+    }
+}

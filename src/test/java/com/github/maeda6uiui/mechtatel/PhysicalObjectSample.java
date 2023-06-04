a404748a341405b1a6b8d09ad086943eff762afd
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-04 14:07:22 +0900

    Add test code

---
+++ src/test/java/com/github/maeda6uiui/mechtatel/PhysicalObjectSample.java
@@ -0,0 +1,154 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.DrawPath;
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.ScreenCreator;
+import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
+import com.github.maeda6uiui.mechtatel.core.component.MttModel3D;
+import com.github.maeda6uiui.mechtatel.core.physics.PhysicalBox3D;
+import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import org.joml.Vector3f;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.github.maeda6uiui.mechtatel.core.util.ClassConversionUtils.convertJOMLVector3fToJMEVector3f;
+
+public class PhysicalObjectSample extends Mechtatel {
+    public PhysicalObjectSample(MttSettings settings) {
+        super(settings);
+    }
+
+    public static void main(String[] args) {
+        MttSettings settings;
+        try {
+            settings = new MttSettings("./Mechtatel/Setting/settings.json");
+        } catch (IOException e) {
+            System.out.println("Failed to load setting file. Use default settings");
+            settings = new MttSettings();
+        }
+
+        new PhysicalObjectSample(settings);
+    }
+
+    private MttScreen mainScreen;
+
+    private MttModel3D plane;
+    private MttModel3D srcCube;
+    private List<PhysicalBox3D> physicalCubes;
+
+    private FreeCamera camera;
+
+    private Random random;
+    private int screenshotCount;
+
+    @Override
+    public void init() {
+        var mainScreenCreator = new ScreenCreator(this, "main");
+        mainScreenCreator.addPostProcessingNabor("spotlight");
+        mainScreenCreator.addPostProcessingNabor("fog");
+        mainScreenCreator.setUseShadowMapping(true);
+        mainScreen = mainScreenCreator.create();
+
+        mainScreen.getFog().setStart(10.0f);
+        mainScreen.getFog().setEnd(20.0f);
+
+        mainScreen.getShadowMappingSettings().setBiasCoefficient(0.003f);
+
+        mainScreen.setSpotlightAmbientColor(new Vector3f(0.0f, 0.0f, 0.0f));
+
+        var spotlightR = mainScreen.createSpotlight();
+        spotlightR.setPosition(new Vector3f(10.0f, 10.0f, 10.0f));
+        spotlightR.setDirection(new Vector3f(-1.0f, -1.0f, -1.0f));
+        spotlightR.setDiffuseColor(new Vector3f(1.0f, 0.0f, 0.0f));
+
+        var spotlightG = mainScreen.createSpotlight();
+        spotlightG.setPosition(new Vector3f(10.0f, 10.0f, -10.0f));
+        spotlightG.setDirection(new Vector3f(-1.0f, -1.0f, 1.0f));
+        spotlightG.setDiffuseColor(new Vector3f(0.0f, 1.0f, 0.0f));
+
+        var spotlightB = mainScreen.createSpotlight();
+        spotlightB.setPosition(new Vector3f(-10.0f, 10.0f, 0.0f));
+        spotlightB.setDirection(new Vector3f(1.0f, -1.0f, 0.0f));
+        spotlightB.setDiffuseColor(new Vector3f(0.0f, 0.0f, 1.0f));
+
+        var drawPath = new DrawPath(this);
+        drawPath.addToScreenDrawOrder("main");
+        drawPath.setPresentScreenName("main");
+        drawPath.apply();
+
+        try {
+            plane = this.createModel3D("main", "./Mechtatel/Standard/Model/Plane/plane.obj");
+
+            srcCube = this.createModel3D("main", "./Mechtatel/Standard/Model/Cube/cube.obj");
+            srcCube.setVisible(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.createPhysicalMesh3D(plane, 0.0f);
+
+        physicalCubes = new ArrayList<>();
+
+        camera = new FreeCamera(mainScreen.getCamera());
+
+        random = new Random();
+        screenshotCount = 0;
+    }
+
+    @Override
+    public void update() {
+        camera.translate(
+                this.getKeyboardPressingCount("W"),
+                this.getKeyboardPressingCount("S"),
+                this.getKeyboardPressingCount("A"),
+                this.getKeyboardPressingCount("D")
+        );
+        camera.rotate(
+                this.getKeyboardPressingCount("UP"),
+                this.getKeyboardPressingCount("DOWN"),
+                this.getKeyboardPressingCount("LEFT"),
+                this.getKeyboardPressingCount("RIGHT")
+        );
+
+        if (this.getKeyboardPressingCount("ENTER") == 1) {
+            var dupCube = this.duplicateModel3D(srcCube);
+            var physicalCube = this.createPhysicalBox3D(1.0f, 1.0f);
+            physicalCube.setComponent(dupCube);
+
+            float x = random.nextFloat();
+            float z = random.nextFloat();
+            int signX = (random.nextInt() % 2 == 0) ? 1 : -1;
+            int signZ = (random.nextInt() % 2 == 0) ? 1 : -1;
+            x *= signX;
+            z *= signZ;
+
+            physicalCube.getBody().setPhysicsLocation(
+                    convertJOMLVector3fToJMEVector3f(new Vector3f(x, 10.0f, z)));
+
+            physicalCubes.add(physicalCube);
+        }
+        if (this.getKeyboardPressingCount("C") == 1) {
+            physicalCubes.forEach(p -> p.cleanup());
+            physicalCubes.clear();
+        }
+    }
+
+    @Override
+    public void postPresent() {
+        if (this.getKeyboardPressingCount("F1") == 1) {
+            try {
+                this.saveScreenshot(
+                        "final",
+                        "bgra",
+                        String.format("screenshot_%d.jpg", screenshotCount));
+                screenshotCount++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

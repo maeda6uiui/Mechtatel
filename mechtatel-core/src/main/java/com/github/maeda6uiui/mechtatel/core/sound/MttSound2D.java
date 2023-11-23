b808f40b69f09e6e1182bf170aa61c1ee9942f8c
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-23 18:20:13 +0900

    Change to URL

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/sound/MttSound2D.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/sound/MttSound2D.java
@@ -4,9 +4,9 @@ import com.goxr3plus.streamplayer.stream.StreamPlayer;
 import com.goxr3plus.streamplayer.stream.StreamPlayerEvent;
 import com.goxr3plus.streamplayer.stream.StreamPlayerException;
 import com.goxr3plus.streamplayer.stream.StreamPlayerListener;
+import jakarta.validation.constraints.NotNull;
 
-import java.io.File;
-import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 
 /**
@@ -15,9 +15,9 @@ import java.util.Map;
  * @author maeda6uiui
  */
 public class MttSound2D extends StreamPlayer implements StreamPlayerListener {
-    public MttSound2D(URI soundResource) throws StreamPlayerException {
+    public MttSound2D(@NotNull URL soundResource) throws StreamPlayerException {
         addStreamPlayerListener(this);
-        open(new File(soundResource));
+        open(soundResource);
     }
 
     @Override

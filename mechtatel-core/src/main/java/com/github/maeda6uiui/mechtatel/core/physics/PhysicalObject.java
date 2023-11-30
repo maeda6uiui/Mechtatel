50c447fedc5b88feccd7e138d9783608fb9c8dd1
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-30 15:16:25 +0900

    Remove unnecessary methods

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalObject.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/physics/PhysicalObject.java
@@ -1,12 +1,7 @@
 package com.github.maeda6uiui.mechtatel.core.physics;
 
-import com.github.maeda6uiui.mechtatel.core.component.MttComponent;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.objects.PhysicsRigidBody;
-import org.joml.Matrix4f;
-
-import static com.github.maeda6uiui.mechtatel.core.util.ClassConversionUtils.convertJMEMatrix3fToJOMLMatrix4f;
-import static com.github.maeda6uiui.mechtatel.core.util.ClassConversionUtils.convertJMEVector3fToJOMLVector3f;
 
 /**
  * Base class for physical objects
@@ -17,19 +12,13 @@ public class PhysicalObject {
     private CollisionShape shape;
     private PhysicsRigidBody body;
 
-    private MttComponent component;
-    private Matrix4f transRotMat;
-
     public PhysicalObject() {
-        transRotMat = new Matrix4f().identity();
+
     }
 
     public void cleanup() {
         if (body != null) {
-            PhysicalObjects.get().ifPresent(p -> p.removeCollisionObject(body));
-        }
-        if (component != null) {
-            component.cleanup();
+            PhysicalObjects.get().ifPresent(v -> v.removeCollisionObject(body));
         }
     }
 
@@ -39,31 +28,14 @@ public class PhysicalObject {
 
     protected void setBody(PhysicsRigidBody body) {
         this.body = body;
-        PhysicalObjects.get().ifPresent(p -> p.addCollisionObject(body));
+        PhysicalObjects.get().ifPresent(v -> v.addCollisionObject(body));
+    }
+
+    public CollisionShape getShape() {
+        return shape;
     }
 
     public PhysicsRigidBody getBody() {
         return body;
-    }
-
-    public void setComponent(MttComponent component) {
-        this.component = component;
-    }
-
-    public void updateObject() {
-        if (body != null) {
-            var bodyLocation = body.getPhysicsLocation(null);
-            var translation = convertJMEVector3fToJOMLVector3f(bodyLocation);
-            var translationMat = new Matrix4f().translate(translation);
-
-            var bodyRotMat = body.getPhysicsRotationMatrix(null);
-            var rotMat = convertJMEMatrix3fToJOMLMatrix4f(bodyRotMat);
-
-            transRotMat = translationMat.mul(rotMat);
-        }
-        if (component != null) {
-            var transRotScaleMat = transRotMat.mul(new Matrix4f().scale(component.getScale()));
-            component.setMat(transRotScaleMat);
-        }
     }
 }

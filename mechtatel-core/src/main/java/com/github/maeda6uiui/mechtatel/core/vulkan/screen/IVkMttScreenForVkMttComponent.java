351e080fad707fc21c85397aec53d0a97fb32d00
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-12-02 11:13:13 +0900

    Add IVkMttScreenForVkMttComponent

---
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/screen/IVkMttScreenForVkMttComponent.java
@@ -0,0 +1,9 @@
+package com.github.maeda6uiui.mechtatel.core.vulkan.screen;
+
+/**
+ * Interface of {@link VkMttScreen} to be used as a reference in Vulkan components
+ *
+ * @author maeda6uiui
+ */
+public interface IVkMttScreenForVkMttComponent {
+}

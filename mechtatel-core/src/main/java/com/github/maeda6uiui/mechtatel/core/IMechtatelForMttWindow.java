711127203c669845b34720adf8ce8c6d34d6b2b2
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-12-10 12:13:57 +0900

    Add comments

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/IMechtatelForMttWindow.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/IMechtatelForMttWindow.java
@@ -6,13 +6,49 @@ package com.github.maeda6uiui.mechtatel.core;
  * @author maeda6uiui
  */
 interface IMechtatelForMttWindow {
+    /**
+     * Called after the window is successfully created.
+     *
+     * @param window Window
+     */
     void init(MttWindow window);
 
+    /**
+     * Called before the clean-up process of the window begins.
+     *
+     * @param window Window
+     */
     void dispose(MttWindow window);
 
+    /**
+     * Called every time the window is resized.
+     * The purpose of this method is to tell the user that the window is resized,
+     * and it is not in sync with the underlying resource recreations.
+     * Use {@link #recreate(MttWindow, int, int)} if you want to run some procedure
+     * <i>after</i> underlying resources are successfully recreated.
+     *
+     * @param window Window
+     * @param width  New width
+     * @param height New height
+     */
     void reshape(MttWindow window, int width, int height);
 
+    /**
+     * Called in each frame after update of resources belonging to the window is completed.
+     *
+     * @param window Window
+     */
     void update(MttWindow window);
 
+    /**
+     * Called after the resource recreations caused by window resize are successfully completed.
+     * Resource recreations take place in the first frame after the window is resized,
+     * therefore the interval of the call to this method is subject to the frames per second of the Mechtatel engine.
+     * Use {@link #reshape(MttWindow, int, int)} if you want to be notified of window resize in realtime.
+     *
+     * @param window Window
+     * @param width  New width
+     * @param height New height
+     */
     void recreate(MttWindow window, int width, int height);
 }

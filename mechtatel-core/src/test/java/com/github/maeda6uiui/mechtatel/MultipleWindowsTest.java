fb22bae7b3846bbe4da6ed13e35135cfded30ff1
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-12-16 19:46:57 +0900

    Add MultipleWindowsTest

---
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/MultipleWindowsTest.java
@@ -0,0 +1,118 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.MttWindow;
+import com.github.maeda6uiui.mechtatel.core.input.keyboard.KeyCode;
+import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
+import org.joml.Vector3f;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
+
+public class MultipleWindowsTest extends Mechtatel {
+    private static final Logger logger = LoggerFactory.getLogger(MultipleWindowsTest.class);
+
+    public MultipleWindowsTest(MttSettings settings) {
+        super(settings);
+        this.run();
+    }
+
+    public static void main(String[] args) {
+        MttSettings
+                .load("./Mechtatel/settings.json")
+                .ifPresentOrElse(
+                        MultipleWindowsTest::new,
+                        () -> logger.error("Failed to load settings")
+                );
+    }
+
+    static class ModelProperties {
+        public MttModel model;
+        public Vector3f position;
+        public Vector3f rotation;
+
+        public ModelProperties() {
+            position = new Vector3f();
+            rotation = new Vector3f();
+        }
+
+        public void apply() {
+            if (model == null) {
+                return;
+            }
+
+            model.translate(position).rotX(rotation.x).rotY(rotation.y).rotZ(rotation.z);
+        }
+    }
+
+    private static final float POSITION_UPDATE_DELTA = 0.05f;
+    private static final float ROTATION_UPDATE_DELTA = 0.05f;
+
+    private Map<String, ModelProperties> modelPropsMap;
+
+    private void createResources(MttWindow window) throws URISyntaxException, IOException {
+        MttScreen defaultScreen = window.getDefaultScreen();
+        MttModel model = defaultScreen.createModel(
+                Objects.requireNonNull(this.getClass().getResource("/Standard/Model/Cube/cube.obj")));
+
+        var random = new Random();
+        var modelPosition = new Vector3f(5.0f, 0.0f, 0.0f);
+        modelPosition.rotateY(random.nextFloat() * (float) Math.PI * 2.0f);
+
+        var modelRotation = new Vector3f(0.0f, 0.0f, 0.0f);
+
+        var modelProps = new ModelProperties();
+        modelProps.model = model;
+        modelProps.position = modelPosition;
+        modelProps.rotation = modelRotation;
+        modelProps.apply();
+
+        modelPropsMap.put(window.getUniqueID(), modelProps);
+    }
+
+    @Override
+    public void onInit() {
+        //Create windows
+        MttSettings settings = MttSettings.get().orElse(new MttSettings());
+
+        var windows = new ArrayList<MttWindow>();
+        for (int i = 0; i < 3; i++) {
+            var window = new MttWindow(this, settings, 640, 480, "Window " + i);
+            windows.add(window);
+        }
+        windows.add(this.getInitialWindow());
+        windows.forEach(this::registerWindow);
+
+        //Create resources
+        modelPropsMap = new HashMap<>();
+        try {
+            for (var window : windows) {
+                this.createResources(window);
+            }
+        } catch (URISyntaxException | IOException e) {
+            logger.error("Error", e);
+            this.closeAllWindows();
+        }
+    }
+
+    @Override
+    public void onUpdate(MttWindow window) {
+        modelPropsMap.values().forEach(modelProps -> {
+            modelProps.position.rotateY(POSITION_UPDATE_DELTA);
+            modelProps.rotation.add(new Vector3f(ROTATION_UPDATE_DELTA));
+            modelProps.apply();
+        });
+
+        MttScreen defaultScreen = window.getDefaultScreen();
+        window.present(defaultScreen);
+
+        if (window.getKeyboardPressingCount(KeyCode.ESCAPE) == 1) {
+            this.closeAllWindows();
+        }
+    }
+}

1c4c5421bd2e865f84b278bb56100720b43141f7
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-03 18:52:03 +0900

    Add ImGuiTest

---
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ImGuiTest.java
@@ -0,0 +1,27 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttImGui;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImGuiTest extends Mechtatel {
+    private static final Logger logger = LoggerFactory.getLogger(ImGuiTest.class);
+
+    public ImGuiTest(MttSettings settings) {
+        super(settings);
+        this.run();
+    }
+
+    public static void main(String[] args) {
+        MttSettings
+                .load("./Mechtatel/settings.json")
+                .ifPresentOrElse(
+                        ImGuiTest::new,
+                        () -> logger.error("Failed to load settings")
+                );
+    }
+
+    private MttImGui imgui;
+}

b7e6addae045187b10befda6577c57aa15c3adc7
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-07 19:02:41 +0900

    Update test code

--- mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ImGuiTest.java
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ImGuiTest.java
@@ -5,14 +5,9 @@ import com.github.maeda6uiui.mechtatel.core.MttSettings;
 import com.github.maeda6uiui.mechtatel.core.MttWindow;
 import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
 import com.github.maeda6uiui.mechtatel.core.screen.component.MttImGui;
-import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
 import imgui.ImGui;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Objects;
 
 public class ImGuiTest extends Mechtatel {
     private static final Logger logger = LoggerFactory.getLogger(ImGuiTest.class);
@@ -32,30 +27,17 @@ public class ImGuiTest extends Mechtatel {
     }
 
     private MttImGui imgui;
-    private MttModel box;
 
     @Override
     public void onInit(MttWindow initialWindow) {
         MttScreen defaultScreen = initialWindow.getDefaultScreen();
         imgui = defaultScreen.createImGui();
-
-        try {
-            box = defaultScreen.createModel(
-                    Objects.requireNonNull(this.getClass().getResource("/Standard/Model/Cube/cube.obj"))
-            );
-        } catch (URISyntaxException | IOException e) {
-            logger.error("Error", e);
-            initialWindow.close();
-        }
     }
 
     @Override
     public void onUpdate(MttWindow window) {
         imgui.declare(() -> {
-            if (ImGui.begin("Window")) {
-                ImGui.text("Hello, world!");
-            }
-            ImGui.end();
+            ImGui.showDemoWindow();
         });
 
         MttScreen defaultScreen = window.getDefaultScreen();

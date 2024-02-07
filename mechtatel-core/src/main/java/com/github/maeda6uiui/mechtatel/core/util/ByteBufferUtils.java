accca1eb332a4e2f324add76eea56e835da5f4d8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-07 21:40:06 +0900

    Add methods to convert vertices to byte buffer

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/util/ByteBufferUtils.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/util/ByteBufferUtils.java
@@ -1,6 +1,11 @@
 package com.github.maeda6uiui.mechtatel.core.util;
 
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttVertex;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttVertex2D;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttVertex2DUV;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttVertexUV;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 
 import java.io.IOException;
@@ -13,6 +18,7 @@ import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Utility methods for ByteBuffer
@@ -57,5 +63,89 @@ public class ByteBufferUtils {
 
         buffer.flip();
         return MemoryUtil.memSlice(buffer);
+    }
+
+    private static ByteBuffer malloc(int bufferSize, MemoryStack stack) {
+        if (stack != null) {
+            return stack.malloc(bufferSize);
+        } else {
+            return MemoryUtil.memAlloc(bufferSize);
+        }
+    }
+
+    /**
+     * Converts a list of {@link MttVertex2D} to a byte buffer.
+     * Buffer is created from a stack memory if stack is provided by the argument.
+     * Otherwise, it is created from a heap memory allocated by {@code malloc()},
+     * and the memory must be freed afterward.
+     *
+     * @param vertices List of vertices
+     * @param stack    Stack
+     * @return Byte buffer
+     */
+    public static ByteBuffer vertices2DToByteBuffer(List<MttVertex2D> vertices, MemoryStack stack) {
+        int bufferSize = MttVertex2D.SIZEOF * vertices.size();
+        ByteBuffer buffer = malloc(bufferSize, stack);
+        vertices.forEach(v -> v.putToByteBuffer(buffer));
+        buffer.flip();
+
+        return buffer;
+    }
+
+    /**
+     * Converts a list of {@link MttVertex2DUV} to a byte buffer.
+     * Buffer is created from a stack memory if stack is provided by the argument.
+     * Otherwise, it is created from a heap memory allocated by {@code malloc()},
+     * and the memory must be freed afterward.
+     *
+     * @param vertices List of vertices
+     * @param stack    Stack
+     * @return Byte buffer
+     */
+    public static ByteBuffer vertices2DUVToByteBuffer(List<MttVertex2DUV> vertices, MemoryStack stack) {
+        int bufferSize = MttVertex2DUV.SIZEOF * vertices.size();
+        ByteBuffer buffer = malloc(bufferSize, stack);
+        vertices.forEach(v -> v.putToByteBuffer(buffer));
+        buffer.flip();
+
+        return buffer;
+    }
+
+    /**
+     * Converts a list of {@link MttVertex} to a byte buffer.
+     * Buffer is created from a stack memory if stack is provided by the argument.
+     * Otherwise, it is created from a heap memory allocated by {@code malloc()},
+     * and the memory must be freed afterward.
+     *
+     * @param vertices List of vertices
+     * @param stack    Stack
+     * @return Byte buffer
+     */
+    public static ByteBuffer verticesToByteBuffer(List<MttVertex> vertices, MemoryStack stack) {
+        int bufferSize = MttVertex.SIZEOF * vertices.size();
+        ByteBuffer buffer = malloc(bufferSize, stack);
+        vertices.forEach(v -> v.putToByteBuffer(buffer));
+        buffer.flip();
+
+        return buffer;
+    }
+
+    /**
+     * Converts a list of {@link MttVertexUV} to a byte buffer.
+     * Buffer is created from a stack memory if stack is provided by the argument.
+     * Otherwise, it is created from a heap memory allocated by {@code malloc()},
+     * and the memory must be freed afterward.
+     *
+     * @param vertices List of vertices
+     * @param stack    Stack
+     * @return Byte buffer
+     */
+    public static ByteBuffer verticesUVToByteBuffer(List<MttVertexUV> vertices, MemoryStack stack) {
+        int bufferSize = MttVertexUV.SIZEOF * vertices.size();
+        ByteBuffer buffer = malloc(bufferSize, stack);
+        vertices.forEach(v -> v.putToByteBuffer(buffer));
+        buffer.flip();
+
+        return buffer;
     }
 }

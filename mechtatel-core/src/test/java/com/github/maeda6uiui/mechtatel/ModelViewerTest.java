e0a2563e8a6f4db39dee8245d7f17b7fb63e0b4a
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-18 19:25:04 +0900

    Show file dialog

--- mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
@@ -7,9 +7,14 @@ import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
 import com.github.maeda6uiui.mechtatel.core.screen.component.MttImGui;
 import imgui.ImGui;
 import imgui.ImGuiIO;
+import imgui.extension.imguifiledialog.ImGuiFileDialog;
+import imgui.extension.imguifiledialog.flag.ImGuiFileDialogFlags;
 import imgui.flag.ImGuiConfigFlags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ModelViewerTest extends Mechtatel {
     private static final Logger logger = LoggerFactory.getLogger(ModelViewerTest.class);
@@ -29,6 +34,7 @@ public class ModelViewerTest extends Mechtatel {
     }
 
     private MttImGui imgui;
+    private Map<String, String> selections;
 
     @Override
     public void onInit(MttWindow initialWindow) {
@@ -40,6 +46,8 @@ public class ModelViewerTest extends Mechtatel {
         io.addConfigFlags(ImGuiConfigFlags.DockingEnable);
 
         ImGui.styleColorsLight();
+
+        selections = new HashMap<>();
     }
 
     @Override
@@ -49,12 +57,24 @@ public class ModelViewerTest extends Mechtatel {
 
             if (ImGui.beginMainMenuBar()) {
                 if (ImGui.beginMenu("File")) {
-                    ImGui.menuItem("Open...", "Ctrl+O");
+                    if (ImGui.menuItem("Open...", "Ctrl+O")) {
+                        ImGuiFileDialog.openModal(
+                                "browse-key",
+                                "Open",
+                                ".bd1,.obj",
+                                ".",
+                                null,
+                                10.0f,
+                                1,
+                                0,
+                                ImGuiFileDialogFlags.None
+                        );
+                    }
                     ImGui.separator();
                     if (ImGui.menuItem("Exit", "Alt+F4")) {
                         window.close();
                     }
-                    
+
                     ImGui.endMenu();
                 }
                 if (ImGui.beginMenu("Tools")) {
@@ -65,7 +85,20 @@ public class ModelViewerTest extends Mechtatel {
 
                 ImGui.endMainMenuBar();
             }
+
+            if (ImGuiFileDialog.display("browse-key", ImGuiFileDialogFlags.None, 700.0f, 400.0f, Float.MAX_VALUE, Float.MAX_VALUE)) {
+                if (ImGuiFileDialog.isOk()) {
+                    selections.putAll(ImGuiFileDialog.getSelection());
+                }
+                ImGuiFileDialog.close();
+            }
         });
+
+        if (!selections.isEmpty()) {
+            logger.info("Selections=");
+            selections.forEach((k, v) -> logger.info("key={},value={}", k, v));
+            selections.clear();
+        }
 
         MttScreen defaultScreen = window.getDefaultScreen();
         defaultScreen.draw();

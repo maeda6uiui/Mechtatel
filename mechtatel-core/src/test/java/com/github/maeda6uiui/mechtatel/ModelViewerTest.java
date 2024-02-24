986645a3981cde0356c1e00bfd864a39f2955d45
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-24 17:27:44 +0900

    Add Rescale dialog

--- mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
@@ -11,6 +11,7 @@ import imgui.extension.imguifiledialog.ImGuiFileDialog;
 import imgui.extension.imguifiledialog.flag.ImGuiFileDialogFlags;
 import imgui.flag.ImGuiConfigFlags;
 import imgui.flag.ImGuiWindowFlags;
+import imgui.type.ImBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +36,18 @@ public class ModelViewerTest extends Mechtatel {
     }
 
     private MttImGui imgui;
+
+    //Selected filepaths
     private Map<String, String> selections;
+
+    //Buffers for values associated with ImGui controls
+    private float[] bufScaleX;
+    private float[] bufScaleY;
+    private float[] bufScaleZ;
+
+    private void resetBuffers() {
+        bufScaleX[0] = bufScaleY[0] = bufScaleZ[0] = 1.0f;
+    }
 
     @Override
     public void onInit(MttWindow initialWindow) {
@@ -49,37 +61,40 @@ public class ModelViewerTest extends Mechtatel {
         ImGui.styleColorsLight();
 
         selections = new HashMap<>();
+
+        bufScaleX = new float[]{1.0f};
+        bufScaleY = new float[]{1.0f};
+        bufScaleZ = new float[]{1.0f};
     }
 
     @Override
     public void onUpdate(MttWindow window) {
         imgui.declare(() -> {
+            boolean shouldOpenRescaleDialog = false;
+
             ImGui.dockSpaceOverViewport(ImGui.getMainViewport());
 
             if (ImGui.beginMainMenuBar()) {
                 if (ImGui.beginMenu("File")) {
-                    if (ImGui.menuItem("Open...", "Ctrl+O")) {
+                    if (ImGui.menuItem("Open...")) {
                         ImGuiFileDialog.openModal(
-                                "browse-key",
-                                "Open",
-                                ".bd1,.obj",
-                                ".",
+                                "browse-key", "Open", ".bd1,.obj", ".",
                                 null,
-                                10.0f,
-                                1,
-                                0,
+                                10.0f, 1, 0,
                                 ImGuiFileDialogFlags.DisableCreateDirectoryButton
                         );
                     }
                     ImGui.separator();
-                    if (ImGui.menuItem("Exit", "Alt+F4")) {
+                    if (ImGui.menuItem("Exit")) {
                         window.close();
                     }
 
                     ImGui.endMenu();
                 }
                 if (ImGui.beginMenu("Tools")) {
-                    ImGui.menuItem("Rescale");
+                    if (ImGui.menuItem("Rescale")) {
+                        shouldOpenRescaleDialog = true;
+                    }
 
                     ImGui.endMenu();
                 }
@@ -90,14 +105,34 @@ public class ModelViewerTest extends Mechtatel {
             if (ImGuiFileDialog.display(
                     "browse-key",
                     ImGuiWindowFlags.NoCollapse,
-                    700.0f,
-                    400.0f,
-                    Float.MAX_VALUE,
-                    Float.MAX_VALUE)) {
+                    700.0f, 400.0f,
+                    Float.MAX_VALUE, Float.MAX_VALUE)) {
                 if (ImGuiFileDialog.isOk()) {
                     selections.putAll(ImGuiFileDialog.getSelection());
                 }
                 ImGuiFileDialog.close();
+            }
+
+            if (shouldOpenRescaleDialog) {
+                ImGui.openPopup("Rescale");
+            }
+            if (ImGui.beginPopupModal("Rescale", new ImBoolean(true), ImGuiWindowFlags.AlwaysAutoResize)) {
+                ImGui.sliderFloat("x", bufScaleX, 0.01f, 10.0f);
+                ImGui.sliderFloat("y", bufScaleY, 0.01f, 10.0f);
+                ImGui.sliderFloat("z", bufScaleZ, 0.01f, 10.0f);
+                ImGui.newLine();
+
+                if (ImGui.button("Cancel")) {
+                    ImGui.closeCurrentPopup();
+                    this.resetBuffers();
+                }
+                ImGui.sameLine();
+                if (ImGui.button("OK")) {
+                    ImGui.closeCurrentPopup();
+                    this.resetBuffers();
+                }
+
+                ImGui.endPopup();
             }
         });
 

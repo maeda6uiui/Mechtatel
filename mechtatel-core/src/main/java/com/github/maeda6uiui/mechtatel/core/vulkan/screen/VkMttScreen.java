9f049b7acfbcca3479213b5a0666f516454dfb0d
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-25 12:10:44 +0900

    Change clear value of stencil

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/screen/VkMttScreen.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/screen/VkMttScreen.java
@@ -747,7 +747,7 @@ public class VkMttScreen implements IVkMttScreenForVkMttTexture, IVkMttScreenFor
             clearValues.get(1).color().float32(stack.floats(0.0f, 0.0f, 0.0f, 0.0f));
             clearValues.get(2).color().float32(stack.floats(0.0f, 0.0f, 0.0f, 0.0f));
             clearValues.get(3).color().float32(stack.floats(0.0f, 0.0f, 0.0f, 0.0f));
-            clearValues.get(4).color().float32(stack.floats(1.0f, 1.0f, 1.0f, 1.0f));
+            clearValues.get(4).color().float32(stack.floats(0.0f, 0.0f, 0.0f, 0.0f));
             renderPassInfo.pClearValues(clearValues);
 
             long mergeScenesInfoUBOMemory = mergeScenesNabor.getUniformBufferMemory(0);

2469a473ecf93b689ac6415fc8d19530e26afdf3
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-02-25 12:11:14 +0900

    Disable color blend in PrimitiveNabor

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PrimitiveNabor.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/nabor/PrimitiveNabor.java
@@ -527,13 +527,7 @@ public class PrimitiveNabor extends Nabor {
                                 VK_COLOR_COMPONENT_G_BIT |
                                 VK_COLOR_COMPONENT_B_BIT |
                                 VK_COLOR_COMPONENT_A_BIT);
-                colorBlendAttachment.blendEnable(true);
-                colorBlendAttachment.srcColorBlendFactor(VK_BLEND_FACTOR_SRC_ALPHA);
-                colorBlendAttachment.dstColorBlendFactor(VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
-                colorBlendAttachment.colorBlendOp(VK_BLEND_OP_ADD);
-                colorBlendAttachment.srcAlphaBlendFactor(VK_BLEND_FACTOR_SRC_ALPHA);
-                colorBlendAttachment.dstAlphaBlendFactor(VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
-                colorBlendAttachment.alphaBlendOp(VK_BLEND_OP_SUBTRACT);
+                colorBlendAttachment.blendEnable(false);
             }
 
             VkPipelineColorBlendStateCreateInfo colorBlending = VkPipelineColorBlendStateCreateInfo.calloc(stack);

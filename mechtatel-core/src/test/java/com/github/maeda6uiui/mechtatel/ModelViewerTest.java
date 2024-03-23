e99276e5a30b827a094be028d65751f6a1b07834
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-03-23 20:59:57 +0900

    Use default screen

--- mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ModelViewerTest.java
@@ -5,14 +5,9 @@ import com.github.maeda6uiui.mechtatel.core.MttSettings;
 import com.github.maeda6uiui.mechtatel.core.MttWindow;
 import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
 import com.github.maeda6uiui.mechtatel.core.input.keyboard.KeyCode;
-import com.github.maeda6uiui.mechtatel.core.operation.BiTextureOperation;
-import com.github.maeda6uiui.mechtatel.core.operation.BiTextureOperationParameters;
 import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
-import com.github.maeda6uiui.mechtatel.core.screen.ScreenImageType;
 import com.github.maeda6uiui.mechtatel.core.screen.component.MttImGui;
 import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
-import com.github.maeda6uiui.mechtatel.core.screen.component.MttTexturedQuad2D;
-import com.github.maeda6uiui.mechtatel.core.screen.texture.MttTexture;
 import imgui.ImGui;
 import imgui.ImGuiIO;
 import imgui.extension.imguifiledialog.ImGuiFileDialog;
@@ -20,7 +15,6 @@ import imgui.extension.imguifiledialog.flag.ImGuiFileDialogFlags;
 import imgui.flag.ImGuiConfigFlags;
 import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
-import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +23,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class ModelViewerTest extends Mechtatel {
     private static final Logger logger = LoggerFactory.getLogger(ModelViewerTest.class);
@@ -58,12 +49,6 @@ public class ModelViewerTest extends Mechtatel {
     private String modelFilepathToLoad;
     private MttModel model;
 
-    private MttScreen imguiScreen;
-    private MttScreen mainScreen;
-    private MttScreen finalScreen;
-    private BiTextureOperation opStencil;
-    private BiTextureOperation opAdd;
-    private MttTexturedQuad2D texturedQuad;
     private FreeCamera camera;
 
     private void resetBuffers() {
@@ -72,17 +57,9 @@ public class ModelViewerTest extends Mechtatel {
 
     @Override
     public void onInit(MttWindow initialWindow) {
-        //Create screen for ImGui components
-        imguiScreen = initialWindow.createScreen(new MttScreen.MttScreenCreateInfo());
-
-        //Create main screen
-        mainScreen = initialWindow.createScreen(new MttScreen.MttScreenCreateInfo());
-
-        //Create final screen that will be presented to the window
-        finalScreen = initialWindow.createScreen(new MttScreen.MttScreenCreateInfo());
-
         //Create ImGui instance for Mechtatel
-        imgui = imguiScreen.createImGui();
+        MttScreen defaultScreen = initialWindow.getDefaultScreen();
+        imgui = defaultScreen.createImGui();
 
         //Add flags to ImGui IO
         imgui.makeCurrent();
@@ -96,35 +73,11 @@ public class ModelViewerTest extends Mechtatel {
 
         modelFilepathToLoad = "";
 
-        try {
-            //Create a textured quad to render to final screen
-            //Texture specified here will be replaced later
-            texturedQuad = finalScreen.createTexturedQuad2D(
-                    Objects.requireNonNull(this.getClass().getResource("/Standard/Texture/checker.png")),
-                    new Vector2f(-1.0f, -1.0f),
-                    new Vector2f(1.0f, 1.0f),
-                    0.0f
-            );
-        } catch (IOException | URISyntaxException e) {
-            logger.error("Error", e);
-            initialWindow.close();
-        }
-
         //Draw axes
-        mainScreen.createLineSet().addPositiveAxes(10.0f).createBuffer();
+        defaultScreen.createLineSet().addPositiveAxes(10.0f).createBuffer();
 
-        //Camera is set on the main screen
-        camera = new FreeCamera(mainScreen.getCamera());
-
-        //Create texture operations
-        this.createTextureOperations();
-    }
-
-    @Override
-    public void onRecreate(MttWindow window, int width, int height) {
-        //Texture operations must be recreated on resource recreation accompanied by window resize,
-        //as some resources such as underlying textures of a screen are destroyed and no longer valid.
-        this.createTextureOperations();
+        //Create camera
+        camera = new FreeCamera(defaultScreen.getCamera());
     }
 
     @Override
@@ -208,6 +161,7 @@ public class ModelViewerTest extends Mechtatel {
         });
 
         //Load model
+        MttScreen defaultScreen = window.getDefaultScreen();
         if (!modelFilepathToLoad.isEmpty() && Files.exists(Paths.get(modelFilepathToLoad))) {
             if (model != null) {
                 model.cleanup();
@@ -215,7 +169,7 @@ public class ModelViewerTest extends Mechtatel {
 
             try {
                 var modelURL = Paths.get(modelFilepathToLoad).toUri().toURL();
-                model = mainScreen.createModel(modelURL);
+                model = defaultScreen.createModel(modelURL);
             } catch (IOException | URISyntaxException e) {
                 logger.error("Error", e);
                 window.close();
@@ -240,60 +194,10 @@ public class ModelViewerTest extends Mechtatel {
                 window.getKeyboardPressingCount(KeyCode.RIGHT)
         );
 
-        //Draw ImGui components
-        imguiScreen.draw();
-        //Draw objects on main screen
-        mainScreen.draw();
+        //Run rendering
+        defaultScreen.draw();
 
-        //Create a stencil
-        //Areas occupied by the objects drawn on the main screen are masked out as 0
-        opStencil.run();
-
-        //Add content of the main screen to the stencil
-        opAdd.run();
-
-        //Render to the final screen and present it
-        finalScreen.draw();
-        window.present(finalScreen);
-    }
-
-    private void createTextureOperations() {
-        //Clean up texture operations if there is any
-        if (opStencil != null) {
-            opStencil.cleanup();
-            opAdd.cleanup();
-        }
-
-        //Create stencil from ImGui and main screens
-        //Mask out the areas covered with main objects from ImGui rendering
-        MttTexture imguiColorTexture = imguiScreen.texturize(ScreenImageType.COLOR, finalScreen);
-        MttTexture mainStencilTexture = mainScreen.texturize(ScreenImageType.STENCIL, finalScreen);
-
-        opStencil = finalScreen.createBiTextureOperation(
-                Arrays.asList(imguiColorTexture, mainStencilTexture),
-                new ArrayList<>(),
-                true
-        );
-
-        var texOpStencilParams = new BiTextureOperationParameters();
-        texOpStencilParams.setOperationType(BiTextureOperationParameters.OperationType.MUL);
-        opStencil.setBiParameters(texOpStencilParams);
-
-        //Add rendering result of main screen to the stencil
-        MttTexture stencilTexture = opStencil.getResultTexture();
-        MttTexture mainColorTexture = mainScreen.texturize(ScreenImageType.COLOR, finalScreen);
-
-        opAdd = finalScreen.createBiTextureOperation(
-                Arrays.asList(stencilTexture, mainColorTexture),
-                new ArrayList<>(),
-                true
-        );
-
-        var texOpAddParams = new BiTextureOperationParameters();
-        texOpAddParams.setOperationType(BiTextureOperationParameters.OperationType.ADD);
-        opAdd.setBiParameters(texOpAddParams);
-
-        //Set result texture of add operation as final output
-        texturedQuad.replaceTexture(opAdd.getResultTexture());
+        //Present to the window
+        window.present(defaultScreen);
     }
 }

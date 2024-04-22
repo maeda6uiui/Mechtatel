e3fbe7844e8b81e19176a6e5adc13f9deefe1a79
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-04-22 21:50:19 +0900

    Add AnimatedFrame and Animation

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/model/MttModelData.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/model/MttModelData.java
@@ -1,6 +1,9 @@
 package com.github.maeda6uiui.mechtatel.core.model;
 
+import org.joml.Matrix4f;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,8 +12,15 @@ import java.util.Map;
  * @author maeda6uiui
  */
 public class MttModelData {
+    public record AnimatedFrame(Matrix4f[] boneMatrices) {
+    }
+
+    public record Animation(String name, double duration, List<AnimatedFrame> frames) {
+    }
+
     public final Map<Integer, MttMaterial> materials;
     public final Map<Integer, MttMesh> meshes;
+    public List<Animation> animationList;
 
     public MttModelData(int numMaterials, int numMeshes) {
         materials = new HashMap<>();

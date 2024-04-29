3b6cc9bc6acd070cbbe5dbe36b26e2f785a4fb75
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-04-29 20:45:09 +0900

    Add bone weights and bone indices

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/screen/component/MttVertex.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/screen/component/MttVertex.java
@@ -14,49 +14,69 @@ public class MttVertex {
     public static final int OFFSETOF_COLOR = OFFSETOF_POS + 3 * Float.BYTES;
     public static final int OFFSETOF_TEXCOORDS = OFFSETOF_COLOR + 4 * Float.BYTES;
     public static final int OFFSETOF_NORMAL = OFFSETOF_TEXCOORDS + 2 * Float.BYTES;
-    public static final int SIZEOF = OFFSETOF_NORMAL + 3 * Float.BYTES;
+    public static final int OFFSETOF_BONE_WEIGHTS = OFFSETOF_NORMAL + 3 * Float.BYTES;
+    public static final int OFFSETOF_BONE_INDICES = OFFSETOF_BONE_WEIGHTS + 4 * Float.BYTES;
+    public static final int SIZEOF = OFFSETOF_BONE_INDICES + 4 * Integer.BYTES;
 
     public Vector3fc pos;
     public Vector4fc color;
     public Vector2fc texCoords;
     public Vector3fc normal;
+    public Vector4fc boneWeights;
+    public Vector4ic boneIndices;
 
     public void putToByteBuffer(ByteBuffer buffer) {
-        buffer.putFloat(pos.x());
-        buffer.putFloat(pos.y());
-        buffer.putFloat(pos.z());
-
-        buffer.putFloat(color.x());
-        buffer.putFloat(color.y());
-        buffer.putFloat(color.z());
-        buffer.putFloat(color.w());
-
-        buffer.putFloat(texCoords.x());
-        buffer.putFloat(texCoords.y());
-
-        buffer.putFloat(normal.x());
-        buffer.putFloat(normal.y());
-        buffer.putFloat(normal.z());
+        for (int i = 0; i < 3; i++) {
+            buffer.putFloat(pos.get(i));
+        }
+        for (int i = 0; i < 4; i++) {
+            buffer.putFloat(color.get(i));
+        }
+        for (int i = 0; i < 2; i++) {
+            buffer.putFloat(texCoords.get(i));
+        }
+        for (int i = 0; i < 3; i++) {
+            buffer.putFloat(normal.get(i));
+        }
+        for (int i = 0; i < 4; i++) {
+            buffer.putFloat(boneWeights.get(i));
+        }
+        for (int i = 0; i < 4; i++) {
+            buffer.putInt(boneIndices.get(i));
+        }
     }
 
-    public MttVertex(Vector3fc pos, Vector4fc color, Vector2fc texCoords, Vector3fc normal) {
+    public MttVertex(
+            Vector3fc pos,
+            Vector4fc color,
+            Vector2fc texCoords,
+            Vector3fc normal,
+            Vector4fc boneWeights,
+            Vector4ic boneIndices) {
         this.pos = pos;
         this.color = color;
         this.texCoords = texCoords;
         this.normal = normal;
+        this.boneWeights = boneWeights;
+        this.boneIndices = boneIndices;
+    }
+
+    public MttVertex(Vector3fc pos, Vector4fc color, Vector2fc texCoords, Vector3fc normal) {
+        this(pos, color, texCoords, normal, new Vector4f(), new Vector4i(-1));
     }
 
     public MttVertex(Vector3fc pos, Vector4fc color, Vector2fc texCoords) {
-        this.pos = pos;
-        this.color = color;
-        this.texCoords = texCoords;
-        this.normal = new Vector3f(0.0f, 1.0f, 0.0f);
+        this(pos, color, texCoords, new Vector3f(0.0f, 1.0f, 0.0f), new Vector4f(), new Vector4i(-1));
     }
 
     public MttVertex() {
-        pos = new Vector3f();
-        color = new Vector4f();
-        texCoords = new Vector2f();
-        normal = new Vector3f(0.0f, 1.0f, 0.0f);
+        this(
+                new Vector3f(),
+                new Vector4f(),
+                new Vector2f(),
+                new Vector3f(0.0f, 1.0f, 0.0f),
+                new Vector4f(),
+                new Vector4i(-1)
+        );
     }
 }

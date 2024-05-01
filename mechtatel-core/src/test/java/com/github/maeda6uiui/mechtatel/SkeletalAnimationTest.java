cd25f972b7edceded45528c4766302f45e0117ce
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-05-01 18:46:34 +0900

    Add test code

---
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/SkeletalAnimationTest.java
@@ -0,0 +1,75 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.MttWindow;
+import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
+import com.github.maeda6uiui.mechtatel.core.input.keyboard.KeyCode;
+import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+public class SkeletalAnimationTest extends Mechtatel {
+    private static final Logger logger = LoggerFactory.getLogger(SkeletalAnimationTest.class);
+
+    public SkeletalAnimationTest(MttSettings settings) {
+        super(settings);
+        this.run();
+    }
+
+    public static void main(String[] args) {
+        MttSettings
+                .load("./Mechtatel/settings.json")
+                .ifPresentOrElse(
+                        SkeletalAnimationTest::new,
+                        () -> logger.error("Failed to load settings")
+                );
+    }
+
+    private FreeCamera camera;
+
+    @Override
+    public void onCreate(MttWindow window) {
+        MttScreen defaultScreen = window.getDefaultScreen();
+        camera = new FreeCamera(defaultScreen.getCamera());
+
+        MttModel animModel;
+        try {
+            animModel = defaultScreen.createModel(
+                    Objects.requireNonNull(this.getClass().getResource("/Standard/Model/Cz1/cz_1.dae"))
+            );
+        } catch (URISyntaxException | IOException e) {
+            logger.error("Error", e);
+            window.close();
+
+            return;
+        }
+
+        defaultScreen.createLineSet().addPositiveAxes(2.0f).createBuffer();
+    }
+
+    @Override
+    public void onUpdate(MttWindow window) {
+        camera.translate(
+                window.getKeyboardPressingCount(KeyCode.W),
+                window.getKeyboardPressingCount(KeyCode.S),
+                window.getKeyboardPressingCount(KeyCode.A),
+                window.getKeyboardPressingCount(KeyCode.D)
+        );
+        camera.rotate(
+                window.getKeyboardPressingCount(KeyCode.UP),
+                window.getKeyboardPressingCount(KeyCode.DOWN),
+                window.getKeyboardPressingCount(KeyCode.LEFT),
+                window.getKeyboardPressingCount(KeyCode.RIGHT)
+        );
+
+        MttScreen defaultScreen = window.getDefaultScreen();
+        defaultScreen.draw();
+        window.present(defaultScreen);
+    }
+}

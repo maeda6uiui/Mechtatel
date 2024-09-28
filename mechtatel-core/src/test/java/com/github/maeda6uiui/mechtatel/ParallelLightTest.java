695d20ae64f6a31f430913c9aad6fe1e6dff6b9e
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-09-28 21:36:17 +0900

    Add test code

---
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/ParallelLightTest.java
@@ -0,0 +1,81 @@
+package com.github.maeda6uiui.mechtatel;
+
+import com.github.maeda6uiui.mechtatel.core.Mechtatel;
+import com.github.maeda6uiui.mechtatel.core.MttSettings;
+import com.github.maeda6uiui.mechtatel.core.MttWindow;
+import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
+import com.github.maeda6uiui.mechtatel.core.input.keyboard.KeyCode;
+import com.github.maeda6uiui.mechtatel.core.postprocessing.PostProcessingProperties;
+import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ParallelLightTest extends Mechtatel {
+    private static final Logger logger = LoggerFactory.getLogger(ParallelLightTest.class);
+
+    public ParallelLightTest(MttSettings settings) {
+        super(settings);
+        this.run();
+    }
+
+    public static void main(String[] args) {
+        MttSettings
+                .load("./Mechtatel/settings.json")
+                .ifPresentOrElse(
+                        ParallelLightTest::new,
+                        () -> logger.error("Failed to load settings")
+                );
+    }
+
+    private MttScreen mainScreen;
+    private MttModel cube;
+    private FreeCamera camera;
+
+    @Override
+    public void onCreate(MttWindow window) {
+        mainScreen = window.createScreen(
+                new MttScreen.MttScreenCreateInfo()
+                        .setPostProcessingNaborNames(List.of("parallel_light"))
+        );
+
+        PostProcessingProperties ppProp = mainScreen.getPostProcessingProperties();
+        ppProp.createParallelLight();
+
+        try {
+            cube = mainScreen.createModel(Paths.get("./Mechtatel/Standard/Model/Cube/cube.obj"));
+        } catch (IOException e) {
+            logger.error("Error", e);
+            window.close();
+
+            return;
+        }
+
+        camera = new FreeCamera(mainScreen.getCamera());
+    }
+
+    @Override
+    public void onUpdate(MttWindow window) {
+        camera.translate(
+                window.getKeyboardPressingCount(KeyCode.W),
+                window.getKeyboardPressingCount(KeyCode.S),
+                window.getKeyboardPressingCount(KeyCode.A),
+                window.getKeyboardPressingCount(KeyCode.D)
+        );
+        camera.rotate(
+                window.getKeyboardPressingCount(KeyCode.UP),
+                window.getKeyboardPressingCount(KeyCode.DOWN),
+                window.getKeyboardPressingCount(KeyCode.LEFT),
+                window.getKeyboardPressingCount(KeyCode.RIGHT)
+        );
+
+        cube.rotY(0.01f);
+
+        mainScreen.draw();
+        window.present(mainScreen);
+    }
+}

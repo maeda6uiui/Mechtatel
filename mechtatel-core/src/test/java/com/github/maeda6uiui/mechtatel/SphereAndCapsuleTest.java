24d87c8e778454bf22c5f1bdb43ad0d132fc10dd
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-09-28 21:06:58 +0900

    Update test code

--- mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/SphereAndCapsuleTest.java
+++ mechtatel-core/src/test/java/com/github/maeda6uiui/mechtatel/SphereAndCapsuleTest.java
@@ -5,11 +5,19 @@ import com.github.maeda6uiui.mechtatel.core.MttSettings;
 import com.github.maeda6uiui.mechtatel.core.MttWindow;
 import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
 import com.github.maeda6uiui.mechtatel.core.input.keyboard.KeyCode;
+import com.github.maeda6uiui.mechtatel.core.postprocessing.PostProcessingProperties;
 import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttCapsule;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttSphere;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class SphereAndCapsuleTest extends Mechtatel {
     private static final Logger logger = LoggerFactory.getLogger(SphereAndCapsuleTest.class);
@@ -28,40 +36,36 @@ public class SphereAndCapsuleTest extends Mechtatel {
                 );
     }
 
+    private MttScreen mainScreen;
     private FreeCamera camera;
+
+    private MttSphere filledSphere;
+    private MttCapsule filledCapsule;
+    private MttModel cube;
 
     @Override
     public void onCreate(MttWindow window) {
-        MttScreen defaultScreen = window.getDefaultScreen();
+        mainScreen = window.createScreen(
+                new MttScreen.MttScreenCreateInfo()
+                        .setPostProcessingNaborNames(List.of("parallel_light"))
+        );
 
-        defaultScreen.createLineSet().addPositiveAxes(10.0f).createBuffer();
-        defaultScreen.createSphere(
-                new Vector3f(0.0f, 0.0f, 0.0f),
-                1.0f,
-                16,
-                16,
-                new Vector4f(1.0f, 1.0f, 1.0f, 1.0f),
-                false
-        );
-        defaultScreen.createCapsule(
-                new Vector3f(3.0f, 0.0f, 0.0f),
-                1.0f,
-                1.0f,
-                16,
-                16,
-                new Vector4f(1.0f, 0.0f, 0.0f, 1.0f),
-                false
-        );
-        defaultScreen.createSphere(
-                new Vector3f(6.0f, 0.0f, 0.0f),
+        PostProcessingProperties ppProp = mainScreen.getPostProcessingProperties();
+        ppProp.createParallelLight();
+
+        mainScreen.createLineSet().addPositiveAxes(10.0f).createBuffer();
+
+        filledSphere = mainScreen.createSphere(
+                new Vector3f(0.0f),
                 1.0f,
                 16,
                 16,
                 new Vector4f(0.0f, 1.0f, 0.0f, 1.0f),
                 true
         );
-        defaultScreen.createCapsule(
-                new Vector3f(9.0f, 0.0f, 0.0f),
+        filledSphere.translate(new Vector3f(3.0f, 0.0f, 0.0f));
+        filledCapsule = mainScreen.createCapsule(
+                new Vector3f(0.0f),
                 1.0f,
                 1.0f,
                 16,
@@ -69,8 +73,19 @@ public class SphereAndCapsuleTest extends Mechtatel {
                 new Vector4f(0.0f, 0.0f, 1.0f, 1.0f),
                 true
         );
+        filledCapsule.translate(new Vector3f(6.0f, 0.0f, 0.0f));
 
-        camera = new FreeCamera(defaultScreen.getCamera());
+        try {
+            cube = mainScreen.createModel(Paths.get("./Mechtatel/Standard/Model/Cube/cube.obj"));
+            cube.translate(new Vector3f(9.0f, 0.0f, 0.0f));
+        } catch (IOException e) {
+            logger.error("Error", e);
+            window.close();
+
+            return;
+        }
+
+        camera = new FreeCamera(mainScreen.getCamera());
     }
 
     @Override
@@ -88,8 +103,10 @@ public class SphereAndCapsuleTest extends Mechtatel {
                 window.getKeyboardPressingCount(KeyCode.RIGHT)
         );
 
-        MttScreen defaultScreen = window.getDefaultScreen();
-        defaultScreen.draw();
-        window.present(defaultScreen);
+        filledCapsule.rotX(0.01f);
+        cube.rotY(0.01f);
+
+        mainScreen.draw();
+        window.present(mainScreen);
     }
 }

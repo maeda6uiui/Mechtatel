37e06e16f23d06dd367d314bc234b198f18d83b0
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-08-27 20:07:08 +0900

    Add module-info.java

---
+++ mechtatel-audio-natives-windows/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.github.maeda6uiui.mechtatel.audio.natives.windows {
+    requires com.github.maeda6uiui.mechtatel.audio.natives;
+
+    opens com.github.maeda6uiui.mechtatel.audio.natives.windows;
+}

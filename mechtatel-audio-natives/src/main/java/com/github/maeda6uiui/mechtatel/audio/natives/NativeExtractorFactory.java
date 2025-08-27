bd3564013c3837afe5e3baa8f616ab3c6c4593de
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-08-27 20:09:57 +0900

    Remove path for macOS

--- mechtatel-audio-natives/src/main/java/com/github/maeda6uiui/mechtatel/audio/natives/NativeExtractorFactory.java
+++ mechtatel-audio-natives/src/main/java/com/github/maeda6uiui/mechtatel/audio/natives/NativeExtractorFactory.java
@@ -11,7 +11,6 @@ public class NativeExtractorFactory {
     private static final String NATIVE_EXTRACTOR_CLASS_NAME = "NativeExtractor";
     private static final String WINDOWS_PACKAGE_PATH = "com.github.maeda6uiui.mechtatel.audio.natives.windows";
     private static final String LINUX_PACKAGE_PATH = "com.github.maeda6uiui.mechtatel.audio.natives.linux";
-    private static final String MACOS_PACKAGE_PATH = "com.github.maeda6uiui.mechtatel.audio.natives.macos";
 
     public static INativeExtractor createNativeExtractor(String platform)
             throws ClassNotFoundException, NoSuchMethodException,
@@ -19,7 +18,6 @@ public class NativeExtractorFactory {
         String className = switch (platform) {
             case "windows" -> WINDOWS_PACKAGE_PATH + "." + NATIVE_EXTRACTOR_CLASS_NAME;
             case "linux" -> LINUX_PACKAGE_PATH + "." + NATIVE_EXTRACTOR_CLASS_NAME;
-            case "macos" -> MACOS_PACKAGE_PATH + "." + NATIVE_EXTRACTOR_CLASS_NAME;
             default -> throw new IllegalArgumentException("Unsupported platform: " + platform);
         };
 

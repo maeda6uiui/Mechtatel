050fc562b45d37e70053ba38b1b694c161c9da96
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-09-21 14:19:28 +0900

    Update method arguments

--- mechtatel-common-utils/src/main/java/com/github/maeda6uiui/mechtatel/common/utils/MttResourceFileUtils.java
+++ mechtatel-common-utils/src/main/java/com/github/maeda6uiui/mechtatel/common/utils/MttResourceFileUtils.java
@@ -12,17 +12,14 @@ public class MttResourceFileUtils {
     /**
      * Extracts a file from a JAR and writes it out to a temporary file.
      *
-     * @param clazz          Class to call {@link Class#getResourceAsStream(String)}
-     * @param filepath       Filepath of the file
-     * @param tempFilePrefix Prefix for the filename of the temporary file
-     * @param tempFileSuffix Suffix for the filename of the temporary file
+     * @param clazz    Class to call {@link Class#getResourceAsStream(String)}
+     * @param filepath Filepath of the file
      * @return Temporary file representing the extracted file
      * @throws IOException If it fails to extract the file
      */
-    public static File extractFile(
-            Class<?> clazz, String filepath, String tempFilePrefix, String tempFileSuffix) throws IOException {
+    public static File extractFile(Class<?> clazz, String filepath) throws IOException {
         try (var bis = new BufferedInputStream(Objects.requireNonNull(clazz.getResourceAsStream(filepath)))) {
-            File tempFile = File.createTempFile(tempFilePrefix, tempFileSuffix);
+            File tempFile = File.createTempFile("mtt", ".tmp");
             tempFile.deleteOnExit();
 
             try (var bos = new BufferedOutputStream(new FileOutputStream(tempFile))) {
@@ -40,15 +37,12 @@ public class MttResourceFileUtils {
     /**
      * Loads a native library contained in a JAR.
      *
-     * @param clazz          Class to call {@link Class#getResourceAsStream(String)}
-     * @param filepath       Filepath of the native library
-     * @param tempFilePrefix Prefix for the filename of the temporary file
-     * @param tempFileSuffix Suffix for the filename of the temporary file
+     * @param clazz    Class to call {@link Class#getResourceAsStream(String)}
+     * @param filepath Filepath of the native library
      * @throws IOException If it fails to extract the file
      */
-    public static void loadNativeLib(
-            Class<?> clazz, String filepath, String tempFilePrefix, String tempFileSuffix) throws IOException {
-        File tempFile = extractFile(clazz, filepath, tempFilePrefix, tempFileSuffix);
+    public static void loadNativeLib(Class<?> clazz, String filepath) throws IOException {
+        File tempFile = extractFile(clazz, filepath);
         System.load(tempFile.getAbsolutePath());
     }
 }

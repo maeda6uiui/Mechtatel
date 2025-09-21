bbc7b9a1bf06c47196ec93bf42921125a1b54880
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-09-21 13:32:54 +0900

    Apply formatter

--- mechtatel-natives/natives/lib-shaderc/mtt-main.h
+++ mechtatel-natives/natives/lib-shaderc/mtt-main.h
@@ -2,37 +2,38 @@
 #include "slang-com-ptr.h"
 #include "slang-com-helper.h"
 
-class MttSlangc{
-    private:
-        Slang::ComPtr<slang::IGlobalSession> globalSession;
+class MttSlangc
+{
+private:
+    Slang::ComPtr<slang::IGlobalSession> globalSession;
 
-        template<class T>
-        T* leak(Slang::ComPtr<slang::IBlob> blob);
-        char* leak_from_str(const char* src);
-    public:
-        MttSlangc();
-        int compile(
-            const char* moduleName,
-            const char* modulePath,
-            const char* source,
-            const char* entryPointName,
-            uint8_t** outSpirv,
-            size_t* outSize,
-            char** outErrorMsg
-        );
+    template <class T>
+    T *leak(Slang::ComPtr<slang::IBlob> blob);
+    char *leak_from_str(const char *src);
 
-        template<class T>
-        void free(const T* p);
+public:
+    MttSlangc();
+    int compile(
+        const char *moduleName,
+        const char *modulePath,
+        const char *source,
+        const char *entryPointName,
+        uint8_t **outSpirv,
+        size_t *outSize,
+        char **outErrorMsg);
+
+    template <class T>
+    void free(const T *p);
 };
 
 //===== C interface =====
 extern "C" int mtt_slangc_compile_to_spirv(
-    const char* moduleName,
-    const char* modulePath,
-    const char* source,
-    const char* entryPointName,
-    uint8_t** outSpirv,
-    size_t* outSize,
-    char** outErrorMsg);
-extern "C" void mtt_slangc_free_uint8_t(const uint8_t* p);
-extern "C" void mtt_slangc_free_str(const char* p);
+    const char *moduleName,
+    const char *modulePath,
+    const char *source,
+    const char *entryPointName,
+    uint8_t **outSpirv,
+    size_t *outSize,
+    char **outErrorMsg);
+extern "C" void mtt_slangc_free_uint8_t(const uint8_t *p);
+extern "C" void mtt_slangc_free_str(const char *p);

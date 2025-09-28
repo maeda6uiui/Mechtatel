1dac8ea5c163e0d33e51024e8788aa173dffa3ef
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-09-28 18:48:54 +0900

    Not delete temp directories in MttSlangcLoader

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/shader/slang/MttSlangcLoader.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/shader/slang/MttSlangcLoader.java
@@ -4,14 +4,14 @@ import com.github.maeda6uiui.mechtatel.natives.IMttNativeLoader;
 import com.github.maeda6uiui.mechtatel.natives.MttNativeLoaderFactory;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Native library loader for the Slang compiler
@@ -31,28 +31,10 @@ class MttSlangcLoader {
             throw new RuntimeException("Unsupported platform");
         }
 
-        final String tmpDirPrefix = "mttnatives";
-
-        //Delete previously created temporary directories
-        Path tempRoot = Paths.get(System.getProperty("java.io.tmpdir"));
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(tempRoot, tmpDirPrefix + "*")) {
-            stream.forEach(p -> {
-                if (Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS) && p.startsWith(tempRoot)) {
-                    try {
-                        FileUtils.deleteDirectory(p.toFile());
-                    } catch (IOException e) {
-                        logger.warn("Failed to delete temporary directory", e);
-                    }
-                }
-            });
-        } catch (IOException e) {
-            logger.warn("Failed to delete temporary directory", e);
-        }
-
         //Create a temporary directory to extract native libs into
         Path tempDir;
         try {
-            tempDir = Files.createTempDirectory(tmpDirPrefix);
+            tempDir = Files.createTempDirectory("mttnatives");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

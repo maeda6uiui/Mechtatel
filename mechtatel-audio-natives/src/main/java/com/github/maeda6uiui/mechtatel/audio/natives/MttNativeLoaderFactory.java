4596ba76ae548ecff5f4ee3973aaeb8a13ae682d
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-09-30 22:14:35 +0900

    Add MttNativeLoaderFactory

---
+++ mechtatel-audio-natives/src/main/java/com/github/maeda6uiui/mechtatel/audio/natives/MttNativeLoaderFactory.java
@@ -0,0 +1,27 @@
+package com.github.maeda6uiui.mechtatel.audio.natives;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Factory for native loader
+ *
+ * @author maeda6uiui
+ */
+public class MttNativeLoaderFactory {
+    private static final String NATIVE_EXTRACTOR_CLASS_NAME = "MttNativeLoader";
+    private static final String WINDOWS_PACKAGE_PATH = "com.github.maeda6uiui.mechtatel.audio.natives.windows";
+    private static final String LINUX_PACKAGE_PATH = "com.github.maeda6uiui.mechtatel.audio.natives.linux";
+
+    public static MttNativeLoaderBase createNativeExtractor(String platform)
+            throws ClassNotFoundException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException, InvocationTargetException {
+        String className = switch (platform) {
+            case "windows" -> WINDOWS_PACKAGE_PATH + "." + NATIVE_EXTRACTOR_CLASS_NAME;
+            case "linux" -> LINUX_PACKAGE_PATH + "." + NATIVE_EXTRACTOR_CLASS_NAME;
+            default -> throw new IllegalArgumentException("Unsupported platform: " + platform);
+        };
+
+        Class<?> clazz = Class.forName(className);
+        return (MttNativeLoaderBase) clazz.getDeclaredConstructor().newInstance();
+    }
+}

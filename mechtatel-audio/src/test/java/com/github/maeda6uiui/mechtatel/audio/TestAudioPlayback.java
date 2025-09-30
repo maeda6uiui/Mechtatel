ae31aa6e151a834fad2c913b6701addb4e904028
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-09-30 22:24:26 +0900

    Rename variables

--- mechtatel-audio/src/test/java/com/github/maeda6uiui/mechtatel/audio/TestAudioPlayback.java
+++ mechtatel-audio/src/test/java/com/github/maeda6uiui/mechtatel/audio/TestAudioPlayback.java
@@ -4,21 +4,21 @@ import java.io.FileNotFoundException;
 
 public class TestAudioPlayback {
     public static void main(String[] args) {
-        MttAudio sound;
+        MttAudio audio;
         try {
-            sound = new MttAudio("./Mechtatel/Standard/Audio/op_8.mp3");
+            audio = new MttAudio("./Mechtatel/Standard/Audio/op_8.mp3");
         } catch (FileNotFoundException e) {
             System.err.println(e);
             return;
         }
 
-        sound.play();
+        audio.play();
 
         while (true) {
-            if (sound.isFinished()) {
+            if (audio.isFinished()) {
                 break;
             }
-            System.out.printf("%f s\n", sound.getPos() / 1000.0);
+            System.out.printf("%f s\n", audio.getPos() / 1000.0);
 
             try {
                 Thread.sleep(1000);

6680dc371aca0796d61b87b0c0fb8738a7600e8d
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-12-22 19:02:23 +0900

    Fix platform argument

--- mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/shader/slang/MttSlangcLoader.java
+++ mechtatel-core/src/main/java/com/github/maeda6uiui/mechtatel/core/vulkan/shader/slang/MttSlangcLoader.java
@@ -1,9 +1,9 @@
 package com.github.maeda6uiui.mechtatel.core.vulkan.shader.slang;
 
+import com.github.maeda6uiui.mechtatel.core.PlatformInfo;
 import com.github.maeda6uiui.mechtatel.natives.MttNativeLoaderBase;
 import com.github.maeda6uiui.mechtatel.natives.MttNativeLoaderFactory2;
 import com.sun.jna.Native;
-import com.sun.jna.Platform;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,20 +16,11 @@ import java.lang.reflect.InvocationTargetException;
  */
 class MttSlangcLoader {
     public static IMttSlangc load() {
-        String platform;
-        if (Platform.isWindows()) {
-            platform = "windows";
-        } else if (Platform.isLinux()) {
-            platform = "linux";
-        } else {
-            throw new RuntimeException("Unsupported platform");
-        }
-
         //Extract native libs
         File slangLibFile;
         File mttslangcLibFile;
         try {
-            MttNativeLoaderBase loader = MttNativeLoaderFactory2.createNativeLoader(platform);
+            MttNativeLoaderBase loader = MttNativeLoaderFactory2.createNativeLoader(PlatformInfo.PLATFORM_WITH_ARCH);
             slangLibFile = loader.extractLibSlang();
             mttslangcLibFile = loader.extractLibMttSlangc();
         } catch (

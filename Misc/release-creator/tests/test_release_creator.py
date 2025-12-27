a2bec9573c9ace40d4b45a249e23ddd10d63ddb8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-12-27 12:32:57 +0900

    Add test case

--- Misc/release-creator/tests/test_release_creator.py
+++ Misc/release-creator/tests/test_release_creator.py
@@ -1,4 +1,5 @@
 import pytest
+import shutil
 from pathlib import Path
 
 from src.release_creator import ReleaseCreator
@@ -76,3 +77,50 @@ def test_release_creator_with_openjdk_cache():
     )
     ret = release_creator.run()
     __common_check(ret)
+
+
+@pytest.mark.order(2)
+def test_release_package_contents():
+    working_dir = Path("./WorkingDir")
+    release_package_file = working_dir.joinpath("mechtatel-hello_linux-x64.tar.gz")
+
+    # Extract release package into Test directory
+    test_dir = working_dir.joinpath("Test")
+    test_dir.mkdir(exist_ok=True)
+    shutil.unpack_archive(release_package_file, test_dir)
+
+    # Check if files exist
+    mechtatel_hello_dir = test_dir.joinpath("mechtatel-hello")
+    assert mechtatel_hello_dir.exists()
+
+    files = mechtatel_hello_dir.glob("*")
+    actual_filenames = [file.name for file in files]
+    actual_filenames = set(actual_filenames)
+    expected_filenames = [
+        "Data",
+        "OpenJDK",
+        "mechtatel-hello_linux-x64.jar",
+        "README.md",
+        "start.bat",
+        "start.sh",
+    ]
+    for filename in expected_filenames:
+        assert filename in actual_filenames
+
+    # Check the content of start scripts
+    bat_command = ".\\OpenJDK\\bin\\java -jar mechtatel-hello_linux-x64.jar %*"
+    bat_file = mechtatel_hello_dir.joinpath("start.bat")
+    with bat_file.open("r", encoding="utf-8") as r:
+        bat_content = r.read()
+
+    assert bat_command in bat_content
+
+    sh_command = "./OpenJDK/bin/java -jar mechtatel-hello_linux-x64.jar $@"
+    sh_file = mechtatel_hello_dir.joinpath("start.sh")
+    with sh_file.open("r", encoding="utf-8") as r:
+        sh_content = r.read()
+
+    assert sh_command in sh_content
+
+    # Remove Test directory
+    shutil.rmtree(test_dir)
